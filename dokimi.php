73a667b371950f79d49f104089edb03ca9349d0a
ipanatsia <ipanatsia@gmail.com>
2020-01-23 12:12:35 +0200

    Delete dokimi.php

--- dokimi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();	
-echo $_SESSION["username"]."<br>". $_SESSION["password"]."<br>".$_SESSION["database"]."<br>".$_SESSION["url"];
-
-//GRANT USAGE ON *.* TO 'ipanatsi'@'localhost';
-//DROP USER 'ipanatsi'@'localhost';
-
-?>
